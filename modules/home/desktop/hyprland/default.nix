334b21abeb6ec80f048ba44c49300701c66dceb6
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2025-12-20 12:56:59 -0300

    disable hyprland wrapping

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -58,6 +58,7 @@ with lib;
           "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
           "col.inactive_border" = "rgba(595959aa)";
           layout = "dwindle";
+          no_focus_fallback = true;
         };
 
         decoration = {
